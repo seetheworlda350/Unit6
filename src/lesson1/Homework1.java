e59d13f94868ac3bfe3213b7ca83b32e3ed1250a
Eshaan Rana <97769730+seetheworlda350@users.noreply.github.com>
2024-12-09 15:02:00 -0800

    Completed unit 6 lesson 1 12/9/2024. Added Homework1 details.

---
+++ src/lesson1/Homework1.java
@@ -0,0 +1,20 @@
+package lesson1;
+
+public class Homework1 {
+    public static void main(String[] args){
+        /* Problem 1 - CodeHS
+        6.1.6 = our first array
+        6.1.7 = Set Scores
+        6.1.8 = Last Element in an Array
+        6.1.9 = Snap Shot Splash Screen
+        6.2.7 = Print Array
+        6.2.8 = Print Odd Array Indices
+        6.2.9 = Find Index of a String
+        6.2.10 = Fibonacci Sequence
+         */
+
+        /* Problem 2 - AP Classroom Videos
+        Watch 6.1 - 6.3 videos
+         */
+    }
+}
---
+++ src/lesson1/Main.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        //create with initial val
+        int[] scores = {47, 82, 36, 95, 74, 64, 53, 92};
+
+        //create with default values
+        String[] defaultScores = new String[8];
+
+        //print the scores
+        for(int i = 0; i<scores.length; i++){
+            System.out.print(scores[i] + " ");
+        }
+    }
+}
