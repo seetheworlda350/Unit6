879fac6556b7409287f9048dfb3d0470f8062af0
Eshaan Rana <seetheworlda350@gmail.com>
2024-12-20 23:56:46 -0800

    Finished array Algorithms

--- src/algorithms/ArrayAlgorithms.java
+++ src/algorithms/ArrayAlgorithms.java
@@ -1,22 +1,24 @@
 package algorithms;
 
+
+import java.util.Arrays;
+
 public class ArrayAlgorithms {
     public static void main(String[] args) {
-    int[] array = {5, 8, -3, 0, 200, -10,5,-3};
-    System.out.println("The smallest number is: " + findMin(array));
-    System.out.println("The largest number is: " + findMax(array));
-    System.out.println("The average is: " + findAverage(array));
-    System.out.println("The array shifted left is: " + shiftLeft(array));
-    System.out.println("The array shifted right is: " + shiftRight(array));
-    System.out.println("The reversed array is: " + reverseArray(array));
-
+        int[] array = {5, 8, -3, 0, 200, -10, 5, -3};
+        System.out.println("The smallest number is: " + findMin(array));
+        System.out.println("The largest number is: " + findMax(array));
+        System.out.println("The average is: " + findAverage(array));
+        System.out.println("The array shifted left is: " + Arrays.toString(shiftLeft(array)));
+        System.out.println("The array shifted right is: " + Arrays.toString(shiftRight(array)));
+        System.out.println("The reversed array is: " + Arrays.toString(reverseArray(array)));
     }
 
     // Finds the minimum value in an array
     public static int findMin(int[] array) {
         int minValue = Integer.MAX_VALUE;
-        for(int num: array){
-            if(num <= minValue){
+        for (int num : array) {
+            if (num <= minValue) {
                 minValue = num;
             }
         }
@@ -26,8 +28,8 @@ public class ArrayAlgorithms {
     // Finds the maximum value in an array
     public static int findMax(int[] array) {
         int maxValue = Integer.MIN_VALUE;
-        for(int num : array){
-            if(num >= maxValue){
+        for (int num : array) {
+            if (num >= maxValue) {
                 maxValue = num;
             }
         }
@@ -38,7 +40,7 @@ public class ArrayAlgorithms {
     public static double findAverage(int[] array) {
         double sum = 0;
         int count = 0;
-        for(double num : array){
+        for (int num : array) {
             sum += num;
             count++;
         }
@@ -48,10 +50,9 @@ public class ArrayAlgorithms {
 
     // Shifts the array to the left by one position
     public static int[] shiftLeft(int[] array) {
-        // TODO: Implement this method
         int[] finalArray = new int[array.length];
-        for(int i = 0; i < array.length; i++){
-            if(i == array.length - 1){
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
                 finalArray[i] = array[0];
             } else {
                 finalArray[i] = array[i + 1];
@@ -62,10 +63,9 @@ public class ArrayAlgorithms {
 
     // Shifts the array to the right by one position
     public static int[] shiftRight(int[] array) {
-        // TODO: Implement this method
         int[] finalArray = new int[array.length];
-        for(int i = 0; i < array.length; i++){
-            if(i == 0){
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0) {
                 finalArray[i] = array[array.length - 1];
             } else {
                 finalArray[i] = array[i - 1];
@@ -76,10 +76,9 @@ public class ArrayAlgorithms {
 
     // Reverses the elements in the array
     public static int[] reverseArray(int[] array) {
-        // TODO: Implement this method
         int[] reversedArray = new int[array.length];
-        for(int i = array.length; i > 0; i--){
-            reversedArray[array.length - i] = array[i - 1];
+        for (int i = array.length - 1; i >= 0; i--) {
+            reversedArray[array.length - 1 - i] = array[i];
         }
         return reversedArray;
     }
